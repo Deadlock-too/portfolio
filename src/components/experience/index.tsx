652297b6aa7c28f13916563b00cfb4ce95579b81
Deadlock-too <craescu.stefangabriel@gmail.com>
2024-04-27 16:43:24 +0200

    Added work experience tags and removed unused institutionLogo

--- src/components/experience/index.tsx
+++ src/components/experience/index.tsx
@@ -1,6 +1,5 @@
 import Boundary from '@/components/boundary'
 import Heading from '@/components/heading'
-import Image, { StaticImageData } from 'next/image'
 
 type ExperienceProps = {
   heading: string
@@ -8,13 +7,12 @@ type ExperienceProps = {
     title: string
     timePeriod: string
     institution: string
-    institutionLogo?: StaticImageData
     description: string
+    tags?: string[]
   }[]
 }
 
-export default function Experience({ heading, items } : ExperienceProps)
- {
+export default function Experience({ heading, items }: ExperienceProps) {
   return (
     <Boundary>
       <Heading as="h2" size="lg" className="focus-in-expand">
@@ -22,24 +20,29 @@ export default function Experience({ heading, items } : ExperienceProps)
       </Heading>
       {
         items.map((item, index) => (
-          <div key={index} className="text-focus-in ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
+          <div key={ index } className="text-focus-in ml-6 mt-8 max-w-prose md:ml-12 md:mt-16">
             <Heading as="h3" size="sm">
               { item.title }
             </Heading>
             <div className="mt-1 flex w-fit items-center gap-1 text-2xl font-semibold tracking-tight text-slate-400">
-              <span>{item.timePeriod}</span>{" "}
-              <span className="text-3xl font-extralight">/</span>{" "}
-              <span>{item.institution}</span>
-              {
-                item.institutionLogo && (
-                  <span>
-                    <Image src={item.institutionLogo} width={80} height={80} alt={`${item.institution} Logo`} />
-                  </span>
-                )
-              }
+              <span>{ item.timePeriod }</span>{ ' ' }
+              <span className="text-3xl font-extralight">/</span>{ ' ' }
+              <span>{ item.institution }</span>
             </div>
-            <div className="prose prrose-lg prose-invert mt-4">
+            <div className="prose prose-lg prose-invert mt-4">
               { item.description }
+            </div>
+            <div className="flex flex-wrap md:justify-start gap-x-4 gap-y-2 text-sky-500 pt-4">
+              {
+                item.tags?.map((tag, index) => (
+                  <span
+                    className="inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-all cursor-default ease-in-out hover:scale-125"
+                    key={ index }
+                  >
+                    { tag }
+                  </span>
+                ))
+              }
             </div>
           </div>
         ))
